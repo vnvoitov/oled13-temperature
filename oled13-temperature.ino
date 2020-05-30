8dee00c106bd7a463135ee4d15a26a20bac7472d
vnvoitov <vnvoitov@gmail.com>
2020-05-30 12:55:03 +0300

    Адреса датчиков заданы константами. Это позволяет избежать переиндексирования при отключении одного из датчиков. Питание датчиков происходит от порта DS_POWER. Питание подается на момент измерения. Это позволяет избежать зависания датчиков.

--- oled13-temperature.ino
+++ oled13-temperature.ino
@@ -1,6 +1,13 @@
 //
 // Определение датчиков температуры и вывод на дисплей их адресов и значений
-//	То же самое выводится в консоль
+// То же самое выводится в консоль
+// Питание датчика динамическое: питание подается перед измерением и снимается сразу после.
+// Так сделано потом, что на длинных линиях датчики сбоят.
+//
+// Адреса датчиков устанавливаются из констант.
+// Если определять их автоматом, то нумерация смещается.
+//
+
 #include "U8glib.h"
 
 U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE);	// I2C 128x64(col2-col129) SH1106,Like HeiTec 1.3' I2C OLED 
@@ -9,7 +16,14 @@ U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE);	// I2C 128x64(col2-col129) SH1106,Li
 #include <DallasTemperature.h>
 
 OneWire ds18x20[] = { 2, 2, 2, 2 };	// порт и ожидаемое количество датчиков
+#define DS_POWER 10
 
+DeviceAddress allThermometers[] = {
+		{ 0x28, 0xAA, 0x72, 0xE5, 0x41, 0x14, 0x01, 0xB8  },// Улица
+		{ 0x28, 0xAA, 0x85, 0xE4, 0x41, 0x14, 0x01, 0xC0  },// Магистраль
+		{ 0x28, 0xAA, 0xBF, 0x3D, 0x42, 0x14, 0x01, 0x8F  },// Газ.котел
+		{ 0x28, 0xAA, 0xBE, 0xE1, 0x41, 0x14, 0x01, 0xB5  },
+};
 const int oneWireCount = sizeof(ds18x20)/sizeof(OneWire);
 DallasTemperature sensor[oneWireCount];
 DeviceAddress temperatureAddress[oneWireCount];
@@ -33,13 +47,14 @@ void draw(DeviceAddress temperatureAddress[], float temperatures[], int oneWireC
 	for (int k=0; k<oneWireCount; k++) {
 
 		u8g.setPrintPos(0, 10+k*12);
-	  
-		for (int i=0; i<sizeof(DeviceAddress); i++) {
-		    if (temperatureAddress[k][i] < 16) u8g.print("0");
-			u8g.print(temperatureAddress[k][i], HEX);
+		if (temperatures[k] != -127) {
+			for (int i=0; i<sizeof(DeviceAddress); i++) {
+				if (temperatureAddress[k][i] < 16) u8g.print("0");
+				u8g.print(temperatureAddress[k][i], HEX);
+			}
+			u8g.print(" ");
+			u8g.print(temperatures[k]);
 		}
-		u8g.print(" ");
-		u8g.print(temperatures[k]);
 	}
 	u8g.setPrintPos(0, 10+oneWireCount*12);
 	u8g.print("Uptime: ");
@@ -48,6 +63,9 @@ void draw(DeviceAddress temperatureAddress[], float temperatures[], int oneWireC
 }
 
 void setup(void) {
+
+	pinMode(DS_POWER, OUTPUT);
+	digitalWrite(DS_POWER, LOW);
 
 	digitalWrite(13, LOW);
 	// start serial port
@@ -65,14 +83,14 @@ void setup(void) {
 	for (int i = 0; i < oneWireCount; i++) {
 		sensor[i].setOneWire(&ds18x20[i]);
 		sensor[i].begin();
-		if (sensor[i].getAddress(deviceAddress, 0)) {
+		// if (sensor[i].getAddress(deviceAddress, 0)) {
 			sensor[i].setResolution(deviceAddress, resolution);
 			for (int z=0; z < sizeof(deviceAddress); z++) {
-				DeviceAddress deviceAddressTMP;
-				sensor[i].getAddress(deviceAddressTMP, i);
-				temperatureAddress[i][z] = deviceAddressTMP[z];
+				// DeviceAddress deviceAddressTMP;
+				// sensor[i].getAddress(deviceAddressTMP, i);
+				temperatureAddress[i][z] = allThermometers[i][z];
 			}
-		}
+		// }
 		Serial.print(i);
 		Serial.print(": ");
 		printAddress(temperatureAddress[i]);
@@ -86,7 +104,9 @@ void loop(void) {
  
     if (millis() - lastTempRequest >= delayInMillis) // waited long enough??
 	{
-		float temperature = sensor[sensorI].getTempCByIndex(sensorI);
+		digitalWrite(DS_POWER, LOW);
+		// float temperature = sensor[sensorI].getTempCByIndex(sensorI);
+		float temperature = sensor[sensorI].getTempC(allThermometers[sensorI]);
 		Serial.print("Temperature for the sensor ");
 		Serial.print(sensorI);
 		Serial.print(" is ");
@@ -97,6 +117,8 @@ void loop(void) {
 		if (sensorI == oneWireCount) {
 			sensorI = 0 ;
 		}
+
+		digitalWrite(DS_POWER, HIGH);
 
 		lastTempRequest = millis();
 	}
